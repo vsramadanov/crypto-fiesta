deafb0e30d0844eabca6aa335d2bd05620d2906c
Vasilii Ramadanov <finimpres@gmail.com>
2021-05-29 22:34:25 +0300

    Move prefix tree to recursive implementation

--- utils/prefix_tree.h
+++ utils/prefix_tree.h
@@ -7,123 +7,118 @@
 
 namespace utils
 {
-namespace
-{
-    int index(char c) { return std::tolower(c) - 'a'; }
-}
 
 class prefix_tree
 {
-    constexpr static int R = 26;
+    constexpr static int R = 'z' - 'a' + 1;
     struct Node
     {
-        bool end;
-        std::array<std::unique_ptr<Node>, R> next;
+        bool is_end;
+        std::array<Node*, R> next;
 
-        Node(bool is_end = false) : end{is_end}, next{} { }
+        Node(bool val = false) : is_end{val}, next{} { }
     };
 
     Node root{};
     size_t N{};
 
-    class iterator
+    int index(char c) { return std::tolower(c) - 'a'; }
+
+    Node* insert(Node* ptr, const std::string& key, size_t k)
     {
-        Node *ptr;
-    public:
-        Node* operator->() { return ptr; }
-        const Node* operator->() const { return ptr; }
-        Node& operator*() { return *ptr; }
-        const Node& operator*() const { return *ptr; }
-
-        bool has_next(char c) { return static_cast<bool>(ptr->next[index(c)]); }
-        iterator next(char c) { return ptr->next[index(c)].get(); }
-        void step(char c) { ptr = ptr->next[index(c)].get(); }
-
-        iterator(Node& node) noexcept { ptr = &node; }
-        iterator(Node* node) noexcept { ptr = node; }
-
-        operator bool() const noexcept { return ptr; }
-    };
-
-    void match(const std::string& prefix, const std::string& str, iterator it, std::vector<std::string>& storage)
-    {
-        if(!it)
-            return;
-
-        for (int k : std::ranges::views::iota(0UL, str.size()))
+        if (ptr == nullptr)
+            ptr = new Node;
+        if (k == key.size())
         {
-            char c = str[k];
-            if (c == '?')
+            if (ptr->is_end == false)
             {
-                for (int j : std::ranges::views::iota(0, R))
-                {
-                    char s = 'a' + j;
-                    std::string new_prefix = prefix + str.substr(0, k) + s;
-                    std::string new_str = str.substr(k + 1);
-                    match(new_prefix, new_str, it.next(s), storage);
-                }
+                ptr->is_end = true;
+                N++;
             }
-            else if(it && it.has_next(c))
-            {
-                it.step(c);
-            }
-            else
-            {
-                return;
-            }
+            return ptr;
         }
-        if (it->end)
-            storage.emplace_back(prefix + str);
+        size_t idx = index(key[k]);
+        ptr->next[idx] = insert(ptr->next[idx], key, k + 1);
+        return ptr;
+    }
+
+    bool contains(Node *ptr, const std::string& key, size_t k)
+    {
+        if (ptr == nullptr)
+            return false;
+        if (k == key.size())
+            return ptr->is_end;
+        size_t idx = index(key[k]);
+        return contains(ptr->next[idx], key, k + 1);
+    }
+
+    void match(const std::string& target, Node* root, size_t k, std::string& key, std::vector<std::string>& storage)
+    {
+        if (root == nullptr)
+            return;
+        if (k == target.size() && root->is_end)
+            storage.push_back(key);
+
+        if (target[k] == '?')
+        {
+            char c = 'a';
+            for (auto* ptr : root->next)
+            {
+                key.push_back(c++);
+                match(target, ptr, k + 1, key, storage);
+                key.pop_back();
+            }
+            return;
+        }
+        char c = target[k];
+        size_t idx = index(c);
+        key.push_back(c);
+        match(target, root->next[idx], k + 1, key, storage);
+        key.pop_back();
+        return;
+    }
+
+    template<typename Func>
+    void visit(Node *root, Func&& f)
+    {
+        if (root == nullptr)
+            return;
+        for (auto* ptr : root->next)
+            visit(ptr, f);
+        f(root);
     }
 
 public:
     prefix_tree() = default;
-    ~prefix_tree() = default;
-    
-    bool insert(const std::string& key)
+    ~prefix_tree()
     {
-        bool out = false;
-        iterator it(root);
-        for (char c : key)
-        {
-            if (std::tolower(c) < 'a' || 'z' < std::tolower(c))
-                return false;
-
-            if(!it.has_next(c))
-                it->next[index(c)] = std::make_unique<Node>();
-            it.step(c);
-        }
-        if (it->end == false)
-        {
-            it->end = true;
-            N++;
-            out = true;
-        }
-        return out;
+        for (auto* ptr : root.next)
+            visit(ptr, [](auto* ptr){ delete ptr; });
+    }
+    
+    void insert(const std::string& key)
+    {
+        insert(&root, key, 0);
     }
     bool contains(const std::string& key)
     {
-        iterator ptr(root);
-        for (char c : key)
-        {
-            if(!ptr.has_next(c))
-                return false;
-            ptr.step(c);
-        }
-        return ptr->end;
+        return contains(&root, key, 0);
     }
-    size_t size() { return N; }
 
     std::vector<std::string> keys()
     {
-
+        std::vector<std::string> res{};
+        return res;
     }
 
-    std::vector<std::string> match(const std::string& str)
+    size_t size() { return N; }
+
+    std::vector<std::string> match(const std::string& target)
     {
         std::vector<std::string> res{};
-        iterator it(root);
-        match("", str, it, res);
+        std::string key;
+        key.reserve(target.size());
+        match(target, &root, 0, key, res);
         return res;
     }
 };
--- utils/ut/prefix_tree_ut.cpp
+++ utils/ut/prefix_tree_ut.cpp
@@ -17,80 +17,81 @@ protected:
 
 TEST_F(PrefixTreeFixture, Insert)
 {
-    EXPECT_TRUE(tree.insert("hello"));
-    EXPECT_FALSE(tree.insert("hello"));
+    tree.insert("hello");
+    tree.insert("hello");
 
     EXPECT_EQ(tree.size(), 1);
-};
+}
 
 TEST_F(PrefixTreeFixture, InsertCaseSense)
 {
-    EXPECT_TRUE(tree.insert("hello"));
-    EXPECT_FALSE(tree.insert("hElLo"));
+    tree.insert("hello");
+    tree.insert("hElLo");
 
     EXPECT_EQ(tree.size(), 1);
-};
+}
 
 TEST_F(PrefixTreeFixture, Contains)
 {
-    EXPECT_TRUE(tree.insert("hello"));
-    EXPECT_TRUE(tree.contains("hello"));
+    tree.insert("hello");
 
+    EXPECT_TRUE(tree.contains("hello"));
     EXPECT_EQ(tree.size(), 1);
-};
+}
 
 TEST_F(PrefixTreeFixture, ContainsPartial)
 {
-    EXPECT_TRUE(tree.insert("hello"));
-    EXPECT_FALSE(tree.contains("hell"));
+    tree.insert("hello");
 
+    EXPECT_FALSE(tree.contains("hell"));
     EXPECT_EQ(tree.size(), 1);
-};
+}
 
 TEST_F(PrefixTreeFixture, Match)
 {
-    EXPECT_TRUE(tree.insert("hello"));
+    tree.insert("hello");
     EXPECT_EQ(tree.match("hell").size(), 0);
-};
+}
 
 TEST_F(PrefixTreeFixture, FullMatchOneWord)
 {
-    EXPECT_TRUE(tree.insert("hello"));
+    tree.insert("hello");
     auto vec = tree.match("hello"); 
     EXPECT_EQ(vec.size(), 1);
 
     EXPECT_STREQ(vec[0].c_str(), "hello");
-};
+}
 
 TEST_F(PrefixTreeFixture, FullMatchWords)
 {
-    EXPECT_TRUE(tree.insert("hello"));
-    EXPECT_TRUE(tree.insert("helfo"));
-    EXPECT_TRUE(tree.insert("heflo"));
+    tree.insert("hello");
+    tree.insert("helfo");
+    tree.insert("heflo");
 
     auto vec = tree.match("hel?o"); 
     EXPECT_EQ(vec.size(), 2);
 
     EXPECT_STREQ(vec[0].c_str(), "helfo");
     EXPECT_STREQ(vec[1].c_str(), "hello");
-};
+}
 
 TEST_F(PrefixTreeFixture, MatchLast)
 {
-    EXPECT_TRUE(tree.insert("hello"));
+    tree.insert("hello");
     
     auto vec = tree.match("hell?"); 
     EXPECT_EQ(vec.size(), 1);
 
     EXPECT_STREQ(vec[0].c_str(), "hello");
-};
+}
 
 TEST_F(PrefixTreeFixture, MultiMatch)
 {
-    EXPECT_TRUE(tree.insert("hello"));
-    EXPECT_TRUE(tree.insert("hell"));
-    EXPECT_TRUE(tree.insert("range"));
-    EXPECT_TRUE(tree.insert("value"));
+    tree.insert("hello");
+    tree.insert("hell");
+    tree.insert("helloworld");
+    tree.insert("range");
+    tree.insert("value");
 
     
     auto vec = tree.match("?????"); 
@@ -99,4 +100,4 @@ TEST_F(PrefixTreeFixture, MultiMatch)
     EXPECT_STREQ(vec[0].c_str(), "hello");
     EXPECT_STREQ(vec[1].c_str(), "range");
     EXPECT_STREQ(vec[2].c_str(), "value");
-};
+}
